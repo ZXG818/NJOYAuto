6057d8f68e7e87eb77f75f81787b9075b126631a
trace-shadow <37297081+trace-shadow@users.noreply.github.com>
2020-09-15 21:06:13 +0800

    Add files via upload

---
+++ ScrapADSdb.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+
+import urllib.request
+import re
+import os
+import time
+
+url = 'https://www-nds.iaea.org/ads/adsace.html'
+
+head = {}
+head['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0'
+
+req = urllib.request.Request(url, headers=head)
+
+response = urllib.request.urlopen(req)
+
+# Data为字符串
+data = response.read().decode('utf-8')
+
+
+# 找到核素名称库
+pattern = re.compile('ACE/(.*?).ZIP', re.DOTALL)
+nuclear = pattern.findall(data)
+print(nuclear)
+print(len(nuclear))
+
+download_links = ['https://www-nds.iaea.org/ads/ACE/' + each + '.ZIP' for each in nuclear]
+
+
+for url in download_links:
+    
+    print(url)
+    
+    req = urllib.request.Request(url, headers=head)
+    response = urllib.request.urlopen(req)
+    
+    data = response.read()    
+    
+    with open(nuclear[download_links.index(url)]+'.ZIP', 'wb') as f:
+        f.write(data)
+    
+    # 睡眠1s
+    time.sleep(1)
+print('DONE')
